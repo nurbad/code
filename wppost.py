730f48835f18a6b236083e0c1bfd5b2cb73c6b71
nurbad <56487301+nurbad@users.noreply.github.com>
2019-10-16 09:08:00 +0700

    post

---
+++ wppost.py
@@ -0,0 +1,15 @@
+from wordpress_xmlrpc import Client
+from wordpress_xmlrpc.methods import posts
+from wordpress_xmlrpc import WordPressPost
+
+your_blog = Client('http://192.81.215.238.com/xmlrpc.php', 'bangnidd', 'ok')
+
+myposts=your_blog.call(posts.GetPosts())
+
+post = WordPressPost()
+post.title = 'MY_POST_TITLE'
+post.slug='MY_POST_PERMANENT_LINK'
+post.content = 'YOUR_POST_CONTENT'
+post.id = your_blog.call(posts.NewPost(post))
+post.post_status = 'publish'
+your_blog.call(posts.EditPost(post.id, post))
